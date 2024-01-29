aefb3c189bcfd89f0d50ceeb4c65d815a4684de6
hideki <rafysta@gmail.com>
2024-01-29 19:49:48 +0900

    update: check package and install

--- install_libraries.R
+++ install_libraries.R
@@ -1,3 +1,8 @@
-install.packages(c("dplyr", "tidyr", "optparse", "data.table"),
-                 repos = "https://cloud.r-project.org",
-                 dependencies = TRUE)
+packages <- c("dplyr", "tidyr", "optparse", "data.table", "RSQLite", "RColorBrewer")
+
+
+for (pkg in packages) {
+  if (!require(pkg, character.only = TRUE)) {
+    install.packages(pkg, repos = "https://cloud.r-project.org", dependencies = TRUE)
+  }
+}
